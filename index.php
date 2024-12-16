b553185029faf958a0163e70dd7881ddc9c76b0d
achraf <achrafsikal@gmail.com>
2024-12-16 10:11:38 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/output.css">
+</head>
+<body>
+    <form action="signup.php" method="post" class="grid w-48 bg-green-300 p-1">
+        <label for="email">email: </label>
+        <input type="email" name="email" id="" required>
+        <label for="pwd">password: </label>
+        <input type="password" name="pwd" id="" required>
+        <input type="submit" value="Singup">
+    </form>
+</body>
+</html>
